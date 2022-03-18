46485e31198ff8fcb29c7f7f7089ab591b279723
JorgeMataH <matahj@gmail.com>
2022-03-18 05:19:19 -0600

    Se terminó el ruteo

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,22 +2,25 @@ const { Router } = require('express');
 const router = Router();
 
 const fs = require('fs');
-const jsons_datos = fs.readFileSync('src/datos.json','utf-8');
+const jsons_datos = fs.readFileSync('src/datos.json', 'utf-8');
 let datos = JSON.parse(jsons_datos);
 
-router.get('/sketches/sk1', (req, res) => {
-    //res.send('Hola mundo');
-    //console.log(info);
-    res.render('sketches/sk1/index.ejs',{datos});
-});
 
-router.get('/sketches/sk2', (req, res) => {
-    res.render('sketches/sk2/index.ejs',{datos});
+
+router.get('/sketches/:id', (req, res, next) => {
+
+    let item = datos.filter(d => d.id === req.params.id);
+
+    if (item.length === 0) {
+        next();
+    } else {
+        let ruta = 'sketches/' + req.params.id + '/index';
+        res.render(ruta, { datos });
+    }
 });
 
 router.get('/', (req, res) => {
-    res.render('sketches/sk1/index.ejs',{datos});
+    res.render('sketches/sk1/index.ejs', { datos });
 });
-
 
 module.exports = router;
